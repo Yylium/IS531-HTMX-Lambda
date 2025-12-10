0b109c3a891c628d10f0ebab7d081fd93c493605
David Bassett <112457273+Yylium@users.noreply.github.com>
2025-12-09 17:27:49 -0700

    initial commit

---
+++ src/main.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+
+
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource("dynamodb")
+    tableName = 'trees-db'
+    table = dynamodb.Table(tableName)
+    all_items = []
+    body = ''
+
+    try:
+        response = table.scan()
+        all_items.extend(response['Items'])
+        print(all_items)
+
+        body += '<div class="grid">'
+        for item in all_items:
+            body += f"""
+            <div class="card">
+                <img src="{item['treeurl']}">
+                <div class="card-body">
+                <div class="card-title">{item['treename']}</div>
+                </div>
+            </div>
+            """
+
+        body += '</div>'
+
+    except Exception as e:
+        print(e)
+        body = 'Error: ' + str(e)
+
+    print(body)
+
+    return {
+        'statusCode': 200,
+        'body': body,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,OPTIONS",
+            "Access-Control-Allow-Headers": "*",
+            "Content-Type": "text/html"
+        }
+    }
+
+
+
+
